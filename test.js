01fb51d157508531cbac17e95e5bc543884e5c68
MaD421 <37409907+MaD421@users.noreply.github.com>
2018-11-06 19:22:11 +0200

    Ver. 0.001

--- test.js
+++ test.js
@@ -1,1 +1,12 @@
-sdsadasdsa
+var http = require('http');
+var uc = require('upper-case');
+var useragent = require('useragent');
+var ip = require("ip");
+useragent(true);
+
+http.createServer(function (req, res) {
+    var agent = useragent.parse(req.headers['user-agent']);
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(ip.address());
+    res.end();
+}).listen(8080);
