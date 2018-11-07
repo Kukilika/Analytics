46b4248c6d4a0ef5773380a439e8932a8f3221b3
MaD421 <37409907+MaD421@users.noreply.github.com>
2018-11-07 08:07:19 +0200

    Ver. 0.002

--- test.js
+++ test.js
@@ -1,12 +1,28 @@
 var http = require('http');
-var uc = require('upper-case');
 var useragent = require('useragent');
 var ip = require("ip");
 useragent(true);
 
 http.createServer(function (req, res) {
-    var agent = useragent.parse(req.headers['user-agent']);
+    var pageStart = Date.now(); // Get current date before the page loads
+    var agent = useragent.parse(req.headers['user-agent']); // Get user info
+
+    var browser = agent.family; // Get user browser
+    var os = agent.os; // Get user OS version
+    var hostIP = ip.address(); // Get user Ip address
+
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(ip.address());
+
+    //Show user information
+    res.write('User browser : ' + browser);
+    res.write('<br/>');
+    res.write('User OS : ' + os);
+    res.write('<br/>');
+    res.write('IP : ' + hostIP);
+    res.write('<br/>');
+    res.write('Page load time: ');
+    var pageEnd = new Date(Date.now() - pageStart); // Get page load time
+    res.write(pageEnd.getMilliseconds() + ' ms');
     res.end();
+
 }).listen(8080);
