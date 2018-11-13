b02ba1c1f6c70cd469cf4bddf849a979c935c784
unknown <andi-mihai.negura@my.fmi.unibuc.ro>
2018-11-13 22:17:53 +0200

    added create endpoint

--- index.js
+++ index.js
@@ -18,17 +18,39 @@ db.on("error",console.log);
 
 var exemplu = new Schema ({ 
     id: Number,
-    name: String,
-    prename: String,
-    phoneNumber: String,
-    steph: Boolean
+    timeOpened: String,
+    timeZone: String,
+    pageon: String,
+    referrer: String,
+    previousSites: String,
+    browserName: String,
+    browserEngine: String,
+    browserVersion1a: String,
+    browserVersion1b: String,
+    browserLanguage: String,
+    browserOnline: String,
+    browserPlatform: String,
+    javaEnabled: String,
+    dataCookiesEnabled: String,
+    dataCookies1: String,
+    dataCookies2: String,
+    dataStorage: String,
+    sizeScreenW: Number,
+    sizeScreenH: Number,
+    sizeDocW: Number,
+    sizeDocH: Number,
+    sizeInW: Number,
+    sizeInH: Number,
+    sizeAvailW: Number,
+    sizeAvailH: Number,
+    scrColorDepth: String,
+    scrPixelDepth: String,
 });
 
 var example = mongoose.model('Example', exemplu);
 
 example.remove({},()=>{
    // var m = new example;
-
     // m.save(function(error){
     //     console.log("your m have been saved");
     //     if(error){
@@ -38,8 +60,16 @@ example.remove({},()=>{
 } );
 
 
+//insert an object to db
+app.post('/create', (req, res)=>{
+    let newObj = new example;
+    newObj.id = 1;
+    Object.assign(newObj, req.body);
+    res.send("Ok");
+})
 
 
+//read
 app.get('/get', function (req, res) {
     mongoose.model('Example').find(function(error,example){
         res.send(example);
@@ -60,6 +90,7 @@ app.post('/collect', (req, res) => {
     console.log(req.body);
     res.json({status:"Ok"});
 })
+
 
 
 app.listen(process.env.PORT || 8080);
